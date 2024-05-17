c739f2589ffce19da7e0bccf75645bbbb3565141
ToyaDR <toya.rosuello@fuzy.ai>
2024-05-17 16:35:52 -0700

    Fix camera movement

--- environments/hub/Hub.cs
+++ environments/hub/Hub.cs
@@ -18,30 +18,14 @@ public partial class Hub : Node3D
 	{
 		Camera3D camera = GetNode("Camera3D") as Camera3D;
 		Camera = camera;
+		Input.MouseMode = Input.MouseModeEnum.Confined;
 	}
 	public override void _Input(InputEvent @event)
 	{
 		if (@event is InputEventMouseMotion)
 		{
 			InputEventMouseMotion mouseEvent = @event as InputEventMouseMotion;
-
-			Vector3 cameraPosition = Camera.GlobalTransform.Origin;
-
-			float translatedX = mouseEvent.Position.X + cameraPosition.X;
-			float translatedY = mouseEvent.Position.Y + cameraPosition.Y;
-
-			float clampedX = mouseEvent.Position.X;
-			float clampedY = mouseEvent.Position.Y;
-			if (translatedX > maxX || translatedX < minX)
-			{
-				clampedX = 0;
-			}
-
-			if (translatedY > maxY || translatedY < minY)
-			{
-				clampedX = 0;
-			}
-			Camera.Translate(new Vector3(clampedX * mouseSensitivityX, -clampedY * mouseSensitivityY, 5.325f));
+			Camera.Translate(new Vector3(mouseEvent.Relative.X * mouseSensitivityX, -mouseEvent.Relative.Y * mouseSensitivityY, 0));
 		}
 	}
 	public override void _Process(double delta)
