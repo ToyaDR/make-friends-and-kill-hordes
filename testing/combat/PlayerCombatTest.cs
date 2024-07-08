c274dcae3de6be7f45f8a8fa0fd67d703e16c0c8
Toya Rosuello <victoria.rosuello@gmail.com>
2024-07-08 12:53:16 -0700

    Sword swing added to combat test

--- testing/combat/PlayerCombatTest.cs
+++ testing/combat/PlayerCombatTest.cs
@@ -6,7 +6,7 @@ public partial class PlayerCombatTest : PlayerCharacter
 	AnimationPlayer animationPlayer;
 	public override void _Ready()
 	{
-		animationPlayer = GetNode<AnimationPlayer>("PlayerCharacterPrefab/pc_arms_rig_v4/AnimationPlayer");
+		animationPlayer = GetNode<AnimationPlayer>("PlayerCharacterPrefab/pc_arms_rig_04/AnimationPlayer");
 		animationPlayer.SpeedScale = 1.5f;
 		ReadyPlayerCharacter();
 	}
@@ -21,7 +21,7 @@ public partial class PlayerCombatTest : PlayerCharacter
 		}
 		if (Input.IsActionJustPressed("attack"))
 		{
-			animationPlayer.Play("pc_anims_v1/playerCharacter_rigAction_001");
+			animationPlayer.Play("swordSwing");
 		}
 	}
 }
