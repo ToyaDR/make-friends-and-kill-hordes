4934c9aeaa153bba6b375b0cd67795e70d581b03
Toya Rosuello <victoria.rosuello@gmail.com>
2024-08-19 10:58:31 -0700

    Lock on

--- environments/grapplePoint/GrapplePointIndicator.cs
+++ environments/grapplePoint/GrapplePointIndicator.cs
@@ -6,11 +6,12 @@ public partial class GrapplePointIndicator : CanvasLayer
 	GrapplePoint grapplePoint;
 	ColorRect lockOn;
 	static Color hoverColor = new(255, 0, 236, 1);
-	static Color defaultColor = new(0, 0, 0, 1);
+	static Color defaultColor = new(0, 0, 0, 0);
 	public override void _Ready()
 	{
 		grapplePoint = GetParent<GrapplePoint>();
 		lockOn = GetChild<Control>(0).GetChild<ColorRect>(0);
+		lockOn.Color = defaultColor;
 	}
 
 	public void SetColor(bool isHovering)
@@ -30,9 +31,8 @@ public partial class GrapplePointIndicator : CanvasLayer
 	{
 		Camera3D camera = GetViewport().GetCamera3D();
 		Vector3 parentPosition = grapplePoint.GlobalPosition;
-		float distance = camera.GlobalPosition.DistanceTo(parentPosition);
 
-		Vector2 unprojectedParentPosition = camera.UnprojectPosition(parentPosition + new Vector3(1 / (distance * distance), 1 / (distance * distance), 1));
+		Vector2 unprojectedParentPosition = camera.UnprojectPosition(parentPosition);
 
 		lockOn.GlobalPosition = unprojectedParentPosition;
 		Visible = !camera.IsPositionBehind(parentPosition);
