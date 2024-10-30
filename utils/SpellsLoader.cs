4808211ddff88cad9d66af085a30da65e4a08dfb
Toya Rosuello <victoria.rosuello@gmail.com>
2024-10-30 15:41:28 -0700

    Spell loader

--- utils/SpellsLoader.cs
+++ utils/SpellsLoader.cs
@@ -1,14 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Godot;
 using Godot.Collections;
 
 public class SpellsLoader
 {
+  private static Godot.Collections.Dictionary<string, Variant> wordsDict;
+  private static Godot.Collections.Dictionary<string, Variant> spellsDict;
+  private static System.Collections.Generic.Dictionary<string, Array<string>> spellToWordsDict;
+
+  private static void PrintDictionary<TKey, TValue>(System.Collections.Generic.Dictionary<TKey, TValue> dict)
+  {
+    GD.Print("========START=======");
+    foreach (var kv in dict)
+    {
+      GD.Print(String.Format("<{0}, {1}>", kv.Key, kv.Value));
+    }
+    GD.Print("========END=======");
+  }
+
   public static void LoadSpells()
   {
-    string wordsFileAsText = FileAccess.GetFileAsString("res://assets/dictionary/spells.json");
-    // string spellsFileAsText = FileAccess.GetFileAsString("res://assets/dictionary/words.json");
-    Dictionary wordsFileAsDict = (Dictionary)Json.ParseString(wordsFileAsText);
+    string wordsFileAsText = FileAccess.GetFileAsString("res://assets/dictionary/words.json");
+    string spellsFileAsText = FileAccess.GetFileAsString("res://assets/dictionary/spells.json");
+    wordsDict = (Godot.Collections.Dictionary<string, Variant>)Json.ParseString(wordsFileAsText);
+    spellsDict = (Godot.Collections.Dictionary<string, Variant>)Json.ParseString(spellsFileAsText);
 
-    GD.Print(wordsFileAsDict);
+    System.Collections.Generic.Dictionary<string, string> wordIdToNameDict = wordsDict.Keys.Aggregate(new System.Collections.Generic.Dictionary<string, string>() { }, (acc, word) =>
+    {
+      Godot.Collections.Dictionary<string, Variant> wordAttributes = (Godot.Collections.Dictionary<string, Variant>)wordsDict.GetValueOrDefault(word);
+
+      string wordId = (string)wordAttributes.GetValueOrDefault("id");
+      acc.Add(wordId, word);
+      return acc;
+    });
+    PrintDictionary(wordIdToNameDict);
+
+    spellToWordsDict = spellsDict.Keys.Aggregate(new System.Collections.Generic.Dictionary<string, Array<string>>() { }, (acc, spell) =>
+    {
+      Godot.Collections.Dictionary<string, Variant> spellAttributes = (Godot.Collections.Dictionary<string, Variant>)spellsDict.GetValueOrDefault(spell);
+
+      Array<string> wordsInSpell = (Array<string>)spellAttributes.GetValueOrDefault("words");
+
+
+      acc.Add(spell, wordsInSpell);
+      return acc;
+    });
+
+    // PrintDictionary(spellToWordsDict);
   }
+
+  // public static string[] GetWordsForSpell(string spellId)
+  // {
+  //   return;
+  //   // return (string[])spellToWordsDict.GetValueOrDefault(spellId);
+  // }
 }
