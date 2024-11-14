3c90b6ad7daf40a163850d18662a1c944fd5ebff
Toya Rosuello <victoria.rosuello@gmail.com>
2024-11-13 16:27:35 -0800

    Load spells/words from json

--- utils/SpellsLoader.cs
+++ utils/SpellsLoader.cs
@@ -7,7 +7,9 @@ using Godot.Collections;
 public class SpellsLoader
 {
   private static Godot.Collections.Dictionary<string, Variant> wordsDict;
+  private static System.Collections.Generic.Dictionary<string, Word> words;
   private static Godot.Collections.Dictionary<string, Variant> spellsDict;
+  private static System.Collections.Generic.Dictionary<string, Spell> spells;
   private static System.Collections.Generic.Dictionary<string, Array<string>> spellToWordsDict;
   private static System.Collections.Generic.Dictionary<string, string> wordIdToNameDict;
 
@@ -19,6 +21,43 @@ public class SpellsLoader
       GD.Print(String.Format("<{0}, {1}>", kv.Key, kv.Value));
     }
     GD.Print("========END=======");
+  }
+
+  private static void SetWords()
+  {
+    words = wordsDict.Keys.Aggregate(new System.Collections.Generic.Dictionary<string, Word>() { }, (acc, word) =>
+    {
+      Godot.Collections.Dictionary<string, Variant> wordAttributes = (Godot.Collections.Dictionary<string, Variant>)wordsDict.GetValueOrDefault(word);
+
+
+      string[] spellList = ((Array<string>)wordAttributes.GetValueOrDefault("spells")).Aggregate(System.Array.Empty<string>(), (spellArr, spellId) =>
+      {
+        return spellArr.Append(spellId).Cast<string>().ToArray();
+      });
+      string rune = (string)wordAttributes.GetValueOrDefault("runePath");
+      string id = (string)wordAttributes.GetValueOrDefault("id");
+      acc.Add(word, new Word(spellList, rune, id, word));
+
+      return acc;
+    });
+  }
+
+  private static void SetSpells()
+  {
+    spells = spellsDict.Keys.Aggregate(new System.Collections.Generic.Dictionary<string, Spell>() { }, (acc, word) =>
+    {
+      Godot.Collections.Dictionary<string, Variant> spellAttributes = (Godot.Collections.Dictionary<string, Variant>)spellsDict.GetValueOrDefault(word);
+
+      string[] wordList = ((Array<string>)spellAttributes.GetValueOrDefault("words")).Aggregate(System.Array.Empty<string>(), (wordArr, wordId) =>
+      {
+        return wordArr.Append(wordId).Cast<string>().ToArray();
+      });
+      string rune = (string)spellAttributes.GetValueOrDefault("runePath");
+      string id = (string)spellAttributes.GetValueOrDefault("id");
+      acc.Add(word, new Spell(wordList, rune, id, word));
+
+      return acc;
+    });
   }
 
   public static void LoadSpells()
@@ -49,6 +88,11 @@ public class SpellsLoader
     });
 
     // PrintDictionary(spellToWordsDict);
+    SetSpells();
+    PrintDictionary(spells);
+
+    SetWords();
+    PrintDictionary(words);
   }
 
   public static Array<string> GetWordsForSpell(string spellId)
